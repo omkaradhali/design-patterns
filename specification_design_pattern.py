96e71908d5edc1f053baf664e4276c88789a1d20
Omkar Adhali <omkar.a2989@gmail.com>
2022-03-26 19:22:41 -0400

    initial commit for design patterns

---
+++ specification_design_pattern.py
@@ -0,0 +1,65 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+## Pattern
+class BaseSpecification:
+    @abstractmethod
+    def is_satisfied_by(self, candidate: Any) -> bool:
+        raise NotImplementedError()
+
+    def and_(self, other: "BaseSpecification") -> "AndSpecification":
+        return AndSpecification(self, other)
+
+    def or_(self, other: "BaseSpecification") -> "OrSpecification":
+        return OrSpecification(self, other)
+
+    def not_(self) -> "NotSpecification":
+        return NotSpecification(self)
+
+@dataclass(frozen=True)
+class AndSpecification(BaseSpecification):
+    first: BaseSpecification
+    second: BaseSpecification
+
+    def is_satisfied_by(self, candidate: Any) -> bool:
+        return self.first.is_satisfied_by(candidate) and self.second.is_satisfied_by(candidate)
+
+@dataclass(frozen=True)
+class OrSpecification(BaseSpecification):
+    first: BaseSpecification
+    second: BaseSpecification
+
+    def is_satisfied_by(self, candidate: Any) -> bool:
+        return self.first.is_satisfied_by(candidate) or self.second.is_satisfied_by(candidate)
+
+@dataclass(frozen=True)
+class NotSpecification(BaseSpecification):
+    subject: BaseSpecification
+
+    def is_satisfied_by(self, candidate: Any) -> bool:
+        return not self.subject.is_satisfied_by(candidate)
+
+# Usage
+class User:
+    def __init__(self, super_user=False):
+        self.super_user = super_user
+
+class UserSpecification(BaseSpecification):
+    def is_satisfied_by(self, candidate):
+        return isinstance(candidate, User)
+
+class SuperUserSpecification(BaseSpecification):
+    def is_satisfied_by(self, candidate):
+        return getattr(candidate, 'super_user', False)
+
+# Execute
+if __name__ == '__main__':
+
+    bob = User()
+    john = User(super_user=True)
+
+    root_specification = UserSpecification().and_(SuperUserSpecification())
+
+    print(root_specification.is_satisfied_by(bob))
+    print(root_specification.is_satisfied_by(john))
---
+++ srp.py
@@ -0,0 +1,51 @@
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.entries.append(f"{self.count}: {text}")
+        self.count += 1
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        print("SELF\n")
+        return "\n".join(self.entries)
+
+    # break SRP
+    def save(self, filename):
+        # file = open(filename, "w")
+        # file.write(str(self))
+        # file.close()
+        print(str(self))
+
+    def load(self, filename):
+        pass
+
+    def load_from_web(self, uri):
+        pass
+
+
+# class PersistenceManager:
+#     def save_to_file(journal, filename):
+#         file = open(filename, "w")
+#         file.write(str(journal))
+#         file.close()
+
+
+if __name__ == "__main__":
+    j = Journal()
+    j.add_entry("I cried today.")
+    j.add_entry("I ate a bug.")
+    j.save("something.txt")
+    print(f"Journal entries:\n{j}\n")
+
+# p = PersistenceManager()
+# file = r'c:\temp\journal.txt'
+# p.save_to_file(j, file)
+
+# verify!
+# with open(file) as fh:
+#     print(fh.read())
