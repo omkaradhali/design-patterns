4ece553f79527aca032eab171df928788d809412
Omkar Adhali <omkar.a2989@gmail.com>
2022-04-18 21:20:14 -0400

    factory pattern

--- Design Patterns/factory_blog.py
+++ Design Patterns/factory_blog.py
@@ -24,9 +24,25 @@ class SMSNotification():
   def send_sms_notification(self):
     print(f"SMS Notification sent to {self.user.first_name} {self.user.last_name} at {self.user.phone_number}")
  
+class NotificationFactory():
+  
+  def __init__(self, type):
+    self.__type = type
+    
+  def get_notification(self):
+    if self.__type == 'EMAIL':
+      return EmailNotification
+    
+    if self.__type == 'SMS':
+      return SMSNotification
+ 
 if __name__ == "__main__":
   user = User("John", "Doe","john_doe@gmail.com", "1234567899")
-  email_notification = EmailNotification(user)
-  sms_notification = SMSNotification(user)
-  email_notification.send_notification()
-  sms_notification.send_sms_notification()
+  # email_notification = EmailNotification(user)
+  # sms_notification = SMSNotification(user)
+  # email_notification.send_notification()
+  # sms_notification.send_sms_notification()
+  
+  notification_class = NotificationFactory('EMAIL').get_notification()
+  email_via_factory = notification_class(user)
+  email_via_factory.send_notification()
